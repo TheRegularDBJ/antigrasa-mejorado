ee3335c76d2f7f6b328c0b9dc0b4297fe60ea81e
TheRegularDBJ <70165606+TheRegularDBJ@users.noreply.github.com>
2021-01-28 17:40:02 -0300

    Create grasa.py

---
+++ grasa.py
@@ -0,0 +1,35 @@
+""""
+
+┏━━━┓━━━━━┏┓━━━┏━━━┓━━━━━━━━━━━━━━━━━┏━━━┓━━┏━━━┓
+┃┏━┓┃━━━━┏┛┗┓━━┃┏━┓┃━━━━━━━━━━━━━━━━━┃┏━┓┃━━┃┏━┓┃
+┃┃━┃┃┏━┓━┗┓┏┛┏┓┃┃━┗┛┏━┓┏━━┓━┏━━┓┏━━┓━┗┛┏┛┃━━┃┃━┃┃
+┃┗━┛┃┃┏┓┓━┃┃━┣┫┃┃┏━┓┃┏┛┗━┓┃━┃━━┫┗━┓┃━┏━┛┏┛━━┃┃━┃┃
+┃┏━┓┃┃┃┃┃━┃┗┓┃┃┃┗┻━┃┃┃━┃┗┛┗┓┣━━┃┃┗┛┗┓┃┃┗━┓┏┓┃┗━┛┃
+┗┛━┗┛┗┛┗┛━┗━┛┗┛┗━━━┛┗┛━┗━━━┛┗━━┛┗━━━┛┗━━━┛┗┛┗━━━┛
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Anti grasa 2.0, elimina la grasa de los servers de discord.
+
+creado por IrregularDBJ#7373
+discord.gg/JsddHWK
+
+"""
+
+
+import discord
+from webserver import keep_alive
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.listen()
+async def on_message(msg):
+    grasa = [":v", ":V", ">:v", ">:V", ':"v', ':"V', ":'v'", ":'V'", "7u7"]
+    for word in grasa:
+        if msg.content.count(word) > 0:
+            await msg.channel.purge(limit=1)
+            await msg.channel.send(f"amenaza eliminada")
+    await bot.process_commands(msg)
+
+bot.run(token=")

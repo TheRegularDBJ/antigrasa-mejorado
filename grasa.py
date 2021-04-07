574bc552d84668abcd1839b0bd39f743766386c4
TheRegularDBJ <70165606+TheRegularDBJ@users.noreply.github.com>
2021-04-07 09:10:17 -0400

    Update grasa.py

--- grasa.py
+++ grasa.py
@@ -17,18 +17,20 @@ discord.gg/JsddHWK
 
 
 import discord
-from discord.ext import commands
 
+class CLientGraso(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
-@bot.listen()
-async def on_message(msg):
+    async def on_message(self, msg):
+        if msg.author == self.user:
+            return
+        
+    async def on_message(self, msg):
     grasa = [":v", ":V", ">:v", ">:V", ':"v', ':"V', ":'v'", ":'V'", "7u7"]
     for word in grasa:
         if msg.content.count(word) > 0:
             await msg.channel.purge(limit=1)
             await msg.channel.send(f"amenaza eliminada")
-    await bot.process_commands(msg)
 
 bot.run(token="")
